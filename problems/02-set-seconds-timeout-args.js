b0e58693c85cabb91911cb5f0c7c81bf0d309baa
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2023-02-02 17:29:11 +0400

    Solved all problems  - added gitignore file.  - solved all four challenges.

--- problems/02-set-seconds-timeout-args.js
+++ problems/02-set-seconds-timeout-args.js
@@ -23,7 +23,8 @@ setSecondsTimeoutArgs(function(arg1, arg2) {
 ***********************************************************************/
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
-  // Your code here
+  let delayInMillisec = delayInSeconds * 1000;
+  setTimeout(cb(...args), delayInMillisec);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
